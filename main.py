5335bab19d553a1cdb071aca25abd07fa2a8f94a
iioannidis94 <109786525+iioannidis94@users.noreply.github.com>
2023-01-25 17:57:10 +0200

    100 movies list

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(URL)
+
+web_resp = response.text
+soup = BeautifulSoup(web_resp, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+top_movies = [title.getText() for title in all_movies]
+print(top_movies)
+movies = top_movies[::-1]
+
+#
+# with open("movies.txt", mode="w", encoding="utf-8") as file:
+#     for movie in movies:
+#         file.write(f"{movie}\n")
