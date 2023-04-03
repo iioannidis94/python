c989a71f86bfd047bd8e6b148fb6fa65c4d45aff
iioannidis94 <109786525+iioannidis94@users.noreply.github.com>
2023-04-03 17:53:43 +0300

    Update data.py

--- UI quizz game/data.py
+++ UI quizz game/data.py
@@ -5,7 +5,7 @@ QUESTION_AMOUNT = 50
 parameters = {
     "amount": QUESTION_AMOUNT,
     "type": "boolean"
-    # "category": 18
+    
 }
 
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
